b20f4c1a5628d5aad1b02478edae97db39c6bd08
Paweł <Pawe@DESKTOP-A3GQSBJ>
2025-05-13 20:08:52 +0200

    Rozwiazanie zadania 03_HumanAge

--- 01_DisplayNumbers/03_HumanAge/Program.cs
+++ 01_DisplayNumbers/03_HumanAge/Program.cs
@@ -10,6 +10,32 @@ namespace _03_HumanAge
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Podaj wiek");
+            int wiek = int.Parse(Console.ReadLine());
+
+            if (wiek <= 0)
+            {
+                Console.WriteLine("Wiek jest nieprawdziwy");
+            }
+            else
+            {
+                if (wiek == 1)
+                {
+                    Console.WriteLine("Bobas");
+                }
+                else if (wiek > 1 && wiek < 18)
+                {
+                    Console.WriteLine("Dziecko");
+                }
+                else if (wiek >= 18 && wiek < 65)
+                {
+                    Console.WriteLine("Dorosly");
+                }
+                else if (wiek >= 65)
+                {
+                    Console.WriteLine("Starzec");
+                }
+            }
         }
     }
 }
